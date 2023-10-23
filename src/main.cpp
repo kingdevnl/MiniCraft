63a4a313b93fc0c85d388381003139549009935f
JasperSmitNL <jasper@kingdev.nl>
2023-10-23 10:40:38 +0200

    ci

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    glfwInit();
+    glfwDefaultWindowHints();
+    auto window = glfwCreateWindow(640, 480, "Hello, World!", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
